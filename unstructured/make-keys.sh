d24b2858da07457fa25809e93337bb3f778203e1
Dmitry Sigaev <www@sigaev.com>
2017-08-23 23:46:27 -0400

    Add digest to openssl commands

--- unstructured/make-keys.sh
+++ unstructured/make-keys.sh
@@ -2,6 +2,7 @@
 
 readonly N_DAYS=365
 readonly N_BITS=2048
+readonly DIGEST=sha256
 readonly KEYS_DIR=${0%make-keys.sh}keys
 
 set -x
@@ -10,16 +11,16 @@ mkdir -p "$KEYS_DIR"
 cd "$KEYS_DIR" || exit 1
 rm -f *.pem
 
-openssl req -new -newkey rsa:$N_BITS -nodes -days $N_DAYS -x509 \
+openssl req -new -newkey rsa:$N_BITS -$DIGEST -nodes -days $N_DAYS -x509 \
     -subj /CN=root \
     -keyout root-key.pem -out root-cert.pem || exit 1
 
 for k in a b; do
-    openssl req -new -newkey rsa:$N_BITS -nodes \
+    openssl req -new -newkey rsa:$N_BITS -$DIGEST -nodes \
         -subj /CN=localhost \
         -keyout $k-key.pem -out $k-csr.pem || exit 1
     openssl x509 -req -CAcreateserial -days $N_DAYS -extfile ../leaf-cert.ext \
-        -CAkey root-key.pem -CA root-cert.pem \
+        -CAkey root-key.pem -CA root-cert.pem -$DIGEST \
         -in $k-csr.pem -out $k-cert.pem || exit 1
     rm -f $k-csr.pem
 done
