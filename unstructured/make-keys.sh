27edb0635d47843955a1e7243b20555034c46e6c
Dmitry Sigaev <www@sigaev.com>
2017-10-07 15:13:06 -0400

    Switch to elliptic-curve TLS

--- unstructured/make-keys.sh
+++ unstructured/make-keys.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 readonly N_DAYS=365
-readonly N_BITS=2048
 readonly DIGEST=sha256
 readonly KEYS_DIR=${0%make-keys.sh}keys
 
@@ -11,12 +10,14 @@ mkdir -p "$KEYS_DIR"
 cd "$KEYS_DIR" || exit 1
 rm -f *.pem
 
-openssl req -new -newkey rsa:$N_BITS -$DIGEST -nodes -days $N_DAYS -x509 \
+openssl req -new -newkey ec:<(openssl ecparam -name prime256v1) \
+    -$DIGEST -nodes -days $N_DAYS -x509 \
     -subj /CN=root \
     -keyout root-key.pem -out root-cert.pem || exit 1
 
 for k in a b; do
-    openssl req -new -newkey rsa:$N_BITS -$DIGEST -nodes \
+    openssl req -new -newkey ec:<(openssl ecparam -name prime256v1) \
+        -$DIGEST -nodes \
         -subj /CN=localhost \
         -keyout $k-key.pem -out $k-csr.pem || exit 1
     openssl x509 -req -CAcreateserial -days $N_DAYS -extfile ../leaf-cert.ext \
